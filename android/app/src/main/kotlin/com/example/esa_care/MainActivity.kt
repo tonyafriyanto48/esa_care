86fcf2f8b75d9708930cb9bbb58cc87b8d916f2a
DESKTOP-J7N1A03\Tony Ajeke <tony.afriyanto48@gmail.com>
2022-06-21 19:54:55 +0700

    no message

---
+++ android/app/src/main/kotlin/com/example/esa_care/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.esa_care
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/welcome.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:esa_care/main.dart';
+
+class welcomepage extends StatefulWidget {
+  const welcomepage();
+
+  @override
+  _welcomepage createState() => _welcomepage();
+}
+
+class _welcomepage extends State<welcomepage> {
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  @override
+  void initState() {
+    super.initState();
+    checkToken();
+  }
+
+  Future<void> checkToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? token = prefs.getString('token');
+    print("token $token");
+    if ((token ?? "") != "") {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => Homescreen(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: new Color(0xFF789FF1),
+              gradient: LinearGradient(
+                colors: [new Color(0xFF5DC6F3), new Color(0xFF0A91E0)],
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage("assets/logo_EJ.png"),
+                width: 300,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15.0),
+              ),
+              SizedBox(width: 400),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.white)),
+                    color: Colors.blue,
+                    minWidth: MediaQuery.of(context).size.width * 0.7,
+                    height: MediaQuery.of(context).size.height * 0.07,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(8.0),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return RegisterPage();
+                      }));
+                    },
+                    child: Text(
+                      "Sign Up".toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FlatButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.white)),
+                    color: Colors.white,
+                    minWidth: MediaQuery.of(context).size.width * 0.7,
+                    height: MediaQuery.of(context).size.height * 0.07,
+                    textColor: Colors.blue,
+                    padding: EdgeInsets.all(8.0),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return LoginPage();
+                      }));
+                    },
+                    child: Text(
+                      "Login".toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Homescreen()));
+                    },
+                    child: Text(
+                      'Lewati',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ), // style: regular16pt.copyWith(color: primaryBlue),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
