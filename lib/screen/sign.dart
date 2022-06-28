459d78dbff907687af1ebffae8596b3d4d1e7c5b
Aulia Alfina Rahmadanti <auliaalfina64@gmail.com>
2022-06-28 23:28:21 +0700

    Add files via upload
    sign terbaru

--- lib/screen/sign.dart
+++ lib/screen/sign.dart
@@ -2,6 +2,112 @@ import 'package:esa_care_fix/screen/register.dart';
 import 'package:flutter/material.dart';
 import 'package:esa_care_fix/constants.dart';
 
+
+final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  String email = "";
+  String password = "";
+  Future<logindao>? _logindao;
+
+  Future<void> setToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("token", token);
+  }
+
+Future<logindao> sendData(String email, String password) async {
+    var formData = FormData.fromMap({
+      'email': email,
+      'pass': password,
+    });
+    var response =
+        await Dio().post('https://api.my.id/ej/masuk.php', data: formData);
+    print(response.data);
+    print(response.statusCode);
+        if (response.statusCode == 200) {
+          // If the server did return a 201 CREATED response,
+          // then parse the JSON.
+          var data = logindao.fromJson(jsonDecode(response.data));
+    print("token ${data.token}");
+    print("token ${data.status}");
+    setToken(data.token ?? "");
+
+      return data;
+       } 
+       else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+         throw Exception('Failed to create album.');
+  }
+  }
+
+   FutureBuilder<logindao> buildFutureBuilder() {
+    return FutureBuilder<logindao>(
+      future: _logindao,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          //return Text(snapshot.data!.title);
+          if (snapshot.data!.token != null) {
+            print("masuk sini pindah route");
+           Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Homescreen(),
+                                  ),
+                                );
+          }
+        } else if (snapshot.hasError) {
+          print("error snapshit");
+        }
+        
+
+        return const CircularProgressIndicator();
+      },
+    );
+  }
+
+   _onCustomAnimationAlertPressed(context) {
+    Alert(
+      context: context,
+      title: "RFLUTTER ALERT",
+      desc: "Flutter is more awesome with RFlutter Alert.",
+      alertAnimation: fadeAlertAnimation,
+    ).show();
+  }
+
+   Widget fadeAlertAnimation(
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return Align(
+      child: FadeTransition(
+        opacity: animation,
+        child: child,
+      ),
+
+    );
+  }
+
+   _onAlertButtonPressed(context) {
+    Alert(
+      context: context,
+      type: AlertType.error,
+      title: "Login Gagal",
+      desc: "silahkan masukan ulang email atau kata sandi anda",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "Oke",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {
+             Navigator.of(context).pop();
+          },
+          width: 120,
+        )
+      ],
+    ).show();
+  }
 class TampilanLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
